8434e1cc0e6a1ce5388abc87846fdd80e026aab0
evgenkolesman <glavstroi_e@mail.ru>
2021-09-21 20:01:12 +0300

    0. Что такое DI? [#6876]

---
+++ src/main/java/di/ConsoleInput.java
@@ -0,0 +1,8 @@
+package di;
+
+public class ConsoleInput {
+
+    public void print(String value) {
+        System.out.println(value);
+    }
+}
--- src/main/java/di/StartUI.java
+++ src/main/java/di/StartUI.java
@@ -1,10 +1,17 @@
 package di;
 
+import java.util.List;
+
 public class StartUI {
     private Store store;
+    private ConsoleInput consoleInput;
 
-    public StartUI(Store store) {
-        this.store = store;
+//    public StartUI(Store store) {
+//        this.store = store;
+//    }
+
+    public StartUI(ConsoleInput consoleInput) {
+        this.consoleInput = consoleInput;
     }
 
     public void add(String value) {
@@ -13,5 +20,14 @@ public class StartUI {
 
     public void print() {
         store.getAll().forEach(System.out :: println);
+    }
+    
+    public void printConsole(String value) {
+        ConsoleInput consoleInput = new ConsoleInput();
+        consoleInput.print(value);
+    }
+
+    public List<String> getAll() {
+        return new Store().getAll();
     }
 }
