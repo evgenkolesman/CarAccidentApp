4f548d615d4fcba612ceff9a020e31137d46fe22
evgenkolesman <glavstroi_e@mail.ru>
2021-09-23 22:55:08 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/di/control/IndexControl.java
+++ src/main/java/di/control/IndexControl.java
@@ -4,11 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.*;
+import java.util.stream.IntStream;
+
 @Controller
 public class IndexControl {
 
+    static List <String> mainList = new ArrayList<>();
     @GetMapping("/")
     public String index(Model model) {
+        List<String> list = new ArrayList<>();
+        model.addAttribute("user", "Evgen Evgenov");
+
+        for (int s = 1; s <= 5; s++) {
+            list.add("user" + s);
+        }
+        model.addAttribute("users", list);
         return "index";
     }
+
+//    public static void main(String[] args) {
+//        for (int s = 1; s <= 5; s++) {
+//            mainList.add("user" + s);
+//        }
+//       IntStream.range(0, mainList.size()).forEach(s -> System.out.println(mainList.get(s)));
+//    }
 }
